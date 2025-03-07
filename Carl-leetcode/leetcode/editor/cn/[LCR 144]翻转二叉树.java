d0b971a6c33cd6b4a62061c2eadc65efa45b9f9f
fengxuegt <fengxuegt@163.com>
2025-03-07 17:34:06 +0800

    add 链表题目

---
+++ Carl-leetcode/leetcode/editor/cn/[LCR 144]翻转二叉树.java
@@ -0,0 +1,53 @@
+//给定一棵二叉树的根节点 root，请左右翻转这棵二叉树，并返回其根节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [5,7,9,8,3,2,4]
+//输出：[5,9,7,4,2,3,8]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 注意：本题与主站 226 题相同：https://leetcode-cn.com/problems/invert-binary-tree/ 
+//
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 392 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode flipTree(TreeNode root) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
--- car-leetcode/src/Main.java
+++ car-leetcode/src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = null;
+        int k = 3;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = node1;
+        while (node1 != null) {
+            ListNode tailNode = dummy;
+            for (int i = 0 ;i < k; i++) {
+                tailNode = tailNode.next;
+                if (tailNode == null) {
+                    // return dummy.next;
+                }
+            }
+            ListNode res[] = reverList(node1, node3);
+        }
+
+        reverList(node1, node3);
+        System.out.println();
+    }
+
+    private static ListNode[] reverList(ListNode head, ListNode tail) {
+        ListNode pre = tail.next;
+        ListNode cur = head;
+        while (pre != tail) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return new ListNode[]{tail, head};
+
     }
 }
