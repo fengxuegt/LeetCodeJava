d5d5ab9c6be1ef01f33cb03c69f1133c7844b8ad
fengxuegt <fengxuegt@163.com>
2025-03-20 09:19:34 +0800

    add leetcode 297
    leetcode 297 二叉树的序列化和反序列化

--- zuo-algorithm/src/Main.java
+++ zuo-algorithm/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String [] sts = new String("absc").split(",");
+        Integer.valueOf(sts[0]);
     }
 }
